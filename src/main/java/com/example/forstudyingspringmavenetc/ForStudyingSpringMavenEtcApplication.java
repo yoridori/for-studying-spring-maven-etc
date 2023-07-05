d5d78d39191467ca1dd687f5d106ed435c9b36f6
yoridori <midori.engineer@gmail.com>
2023-07-05 23:05:51 +0900

    初回コミット

---
+++ src/main/java/com/example/forstudyingspringmavenetc/ForStudyingSpringMavenEtcApplication.java
@@ -0,0 +1,13 @@
+package com.example.forstudyingspringmavenetc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForStudyingSpringMavenEtcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForStudyingSpringMavenEtcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/forstudyingspringmavenetc/ForStudyingSpringMavenEtcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.forstudyingspringmavenetc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForStudyingSpringMavenEtcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
